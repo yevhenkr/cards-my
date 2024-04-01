985a73069bfa7659dc7f84c78f857c152878ef20
Eugen <vasilenkoevgen88@gmail.com>
2024-04-01 11:26:32 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import '@/app/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
