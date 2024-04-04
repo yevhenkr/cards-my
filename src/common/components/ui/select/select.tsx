527f55c7bd11d79bfa8a204b8a19fd4eff3c9e82
Eugen <vasilenkoevgen88@gmail.com>
2024-04-04 15:12:55 +0300

    fetch: added pagination

---
+++ src/common/components/ui/select/select.tsx
@@ -0,0 +1,98 @@
+import { CSSProperties, ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from 'react'
+
+import { ArrowDownIcon } from '@/assets/icons/arrowDownIcon'
+import { ArrowUpIcon } from '@/assets/icons/arrowUpIcon'
+import * as SelectRadix from '@radix-ui/react-select'
+
+import s from '@/common/components/ui/select/select.module.scss'
+
+import { Typography } from '../typography'
+
+export type OptionType = {
+  title: string
+  value: string
+}
+export type SelectProps = {
+  className?: string
+  disabled?: boolean
+  label?: string
+  options: OptionType[]
+  styleForContent?: CSSProperties
+  styleForItem?: CSSProperties
+  styleForTrigger?: CSSProperties
+} & ComponentPropsWithoutRef<typeof SelectRadix.Root>
+
+export const MySelect = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectProps>(
+  (
+    {
+      className,
+      disabled,
+      label,
+      options,
+      styleForContent,
+      styleForItem,
+      styleForTrigger,
+      ...restProps
+    },
+    ref
+  ) => {
+    const [open, setOpen] = useState(false)
+
+    return (
+      <>
+        <SelectRadix.Root onOpenChange={() => setOpen(!open)} {...restProps}>
+          <div className={s.selectRoot}>
+            {label && (
+              <Typography
+                as={'label'}
+                className={disabled ? s.labelDisabled : s.label}
+                variant={'body2'}
+              >
+                {label}
+              </Typography>
+            )}
+
+            <SelectRadix.Trigger
+              className={`${s.selectTrigger} ${className ?? ''}`}
+              disabled={disabled}
+              ref={ref}
+              style={styleForTrigger}
+            >
+              <SelectRadix.Value
+                className={s.selectValue}
+                defaultValue={options[0].value}
+                placeholder={options[0].title}
+              />
+              {open ? (
+                <ArrowUpIcon className={s.selectIcon} />
+              ) : (
+                <ArrowDownIcon className={s.selectIcon} />
+              )}
+            </SelectRadix.Trigger>
+
+            <SelectRadix.Content
+              className={s.selectContent}
+              position={'popper'}
+              style={styleForContent}
+            >
+              <SelectRadix.Viewport className={s.SelectViewport}>
+                {options.map((option, index) => {
+                  return (
+                    <SelectRadix.Item
+                      className={s.selectItem}
+                      key={index}
+                      style={styleForItem}
+                      value={option.value}
+                    >
+                      <SelectRadix.ItemText>{option.title}</SelectRadix.ItemText>
+                    </SelectRadix.Item>
+                  )
+                })}
+              </SelectRadix.Viewport>
+            </SelectRadix.Content>
+          </div>
+        </SelectRadix.Root>
+      </>
+    )
+  }
+)
